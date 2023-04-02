aa1a8ff586e02ee4e50ff41ad6497615b39a3b39
Anita de Prado <anandromeda@gmail.com>
2023-04-02 21:06:41 +0200

    🚧 add first arduino code to activate rele depending on the % humidity

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -9,28 +9,41 @@
 // ----------------------------------
 
 // Mapeo a % humedad
-const int dry = 940; // Este valor se considerará 0% humedad
-const int wet = 800; // Este valor se considerará 100% humedad
+const int dry = 920; // Este valor se considerará 0% humedad
+const int wet = 820; // Este valor se considerará 100% humedad
+
+const int RELAY_PIN = 3;  
 
 void setup() {
-  Serial.begin(9600);
+    pinMode(RELAY_PIN, OUTPUT); // initialize digital pin as an output.
+    Serial.begin(9600);
 }
 
 void loop() {
 
-  // Read sensor val
-  int sensorVal_A0 = analogRead(A0);
-  int sensorVal_A1 = analogRead(A1);
+    // Read sensor val
+    int sensorVal_A0 = analogRead(A0);
+    int sensorVal_A1 = analogRead(A1);
 
-  // Calculate % humidity
-  int percentageHumididy_A0 = map(sensorVal_A0, wet, dry, 100, 0); 
-  int percentageHumididy_A1 = map(sensorVal_A1, wet, dry, 100, 0);
- 
-  // Print values
-  String output_A0 = String(sensorVal_A0) + " (" + String(percentageHumididy_A0) + "%)";
-  String output_A1 = String(sensorVal_A1) + " (" + String(percentageHumididy_A1) + "%)";
-  String output = output_A0 + " - " + output_A1;
-  Serial.println(output);
-  delay(500);
+    // Calculate % humidity
+    int percentageHumididy_A0 = map(sensorVal_A0, wet, dry, 100, 0); 
+    int percentageHumididy_A1 = map(sensorVal_A1, wet, dry, 100, 0);
+
+    // Activate or not the rele
+    if ((percentageHumididy_A0 < 40) and (percentageHumididy_A0 < 40)) {
+        digitalWrite(RELAY_PIN, LOW);
+        // Serial.println("ON");
+    }
+    else{
+        digitalWrite(RELAY_PIN, HIGH);
+        //Serial.println("OFF");
+    }
+    
+    // Print values
+    String output_A0 = String(sensorVal_A0) + " (" + String(percentageHumididy_A0) + "%)";
+    String output_A1 = String(sensorVal_A1) + " (" + String(percentageHumididy_A1) + "%)";
+    String output = output_A0 + " - " + output_A1;
+    Serial.println(output);
+    delay(500);
 
 }
