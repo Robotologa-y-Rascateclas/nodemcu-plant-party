f8ca8c2ef1e8d4701ff1b2873bc2d0a552bc08fe
Sergio Ruiz <serginator@gmail.com>
2023-04-16 02:36:17 +0200

    :rocket: reduce deepsleep to 10 minutes

--- esp8266/src/config.h
+++ esp8266/src/config.h
@@ -19,4 +19,4 @@ const int DELAY_MEASURE_TIME = 600000;
 
 // Time to deepSleep (max 71 minutes, 4260e6)
 // 3600e6 = 1h | 600e6 = 10min | 300e6 = 5min | 60e6 = 1min
-const int DELAY_DEEPSLEEP_TIME = 1800e6;
+const int DELAY_DEEPSLEEP_TIME = 600e6;
