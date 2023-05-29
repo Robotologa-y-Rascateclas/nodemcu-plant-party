901e137975f9d43db338b5a52ba79dfad590630f
Sergio Ruiz <serginator@gmail.com>
2023-05-29 20:59:16 +0200

    :robot: add influxdb, chronograf and grafana to docker-compose.yml and send sensors data from esp32

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -50,55 +50,40 @@ int getSensorValue(int sensorPin) {
 
 void loop() {
   // Read the moisture level from the sensors
-  int sensor1value = getSensorValue(36);
-  int sensor2value = getSensorValue(39);
-  int sensor3value = getSensorValue(34);
-  int sensor4value = getSensorValue(35);
-  int sensor5value = getSensorValue(32);
+  int sensor1value = getSensorValue(SENSOR_1_PIN);
+  int sensor2value = getSensorValue(SENSOR_2_PIN);
+  int sensor3value = getSensorValue(SENSOR_3_PIN);
+  int sensor4value = getSensorValue(SENSOR_4_PIN);
+  int sensor5value = getSensorValue(SENSOR_5_PIN);
 
   // float moisture = map(sensorValue, 1023, 0, 0, 100) / 100.0;
 
-  // Create a JSON array to hold the sensors data
-  StaticJsonDocument<500> doc;
-  JsonArray sensors = doc.to<JsonArray>();
+  // Send sensors data to nodejs server by WiFi
+  sendSensorData(
+    sensor1value,
+    sensor2value,
+    sensor3value,
+    sensor4value,
+    sensor5value
+  );
 
-  // Create the first sensor object
-  JsonObject sensor1 = sensors.createNestedObject();
-  sensor1["sensor"] = "sensor_1";
-  JsonObject data1 = sensor1.createNestedObject("data");
-  data1["moisture"] = sensor1value;
+  sendBotMessage(
+    sensor1value,
+    sensor2value,
+    sensor3value,
+    sensor4value,
+    sensor5value
+  );
 
-  // Create the second sensor object
-  JsonObject sensor2 = sensors.createNestedObject();
-  sensor2["sensor"] = "sensor_2";
-  JsonObject data2 = sensor2.createNestedObject("data");
-  data2["moisture"] = sensor2value;
-
-  // Create the third sensor object
-  JsonObject sensor3 = sensors.createNestedObject();
-  sensor3["sensor"] = "sensor_3";
-  JsonObject data3 = sensor3.createNestedObject("data");
-  data3["moisture"] = sensor3value;
-
-  // Create the fourth sensor object
-  JsonObject sensor4 = sensors.createNestedObject();
-  sensor4["sensor"] = "sensor_4";
-  JsonObject data4 = sensor4.createNestedObject("data");
-  data4["moisture"] = sensor4value;
-
-  // Create the fifth sensor object
-  JsonObject sensor5 = sensors.createNestedObject();
-  sensor5["sensor"] = "sensor_5";
-  JsonObject data5 = sensor5.createNestedObject("data");
-  data5["moisture"] = sensor5value;
-
-  String payload;
-  serializeJson(doc, payload);
-
-  // Send sensors data to server by WiFi
-  sendSensorData(payload);
-
-  sendBotMessage(sensor1value, sensor2value, sensor3value, sensor4value, sensor5value);
+  // Send sensors data to InfluxDB
+  std::vector<int> sensorValues = {
+    sensor1value,
+    sensor2value,
+    sensor3value,
+    sensor4value,
+    sensor5value
+  };
+  sendSensorDataToInfluxDB(sensorValues);
 
   // Print payload in serial
   // Serial.println(payload);
--- esp32/src/net.h
+++ esp32/src/net.h
@@ -7,6 +7,9 @@
 // Load config variables
 #include <configNet.h>
 
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+
 const unsigned long BOT_MTBS = 1000; // mean time between scan messages
 unsigned long bot_lasttime; // last time messages' scan has been done
 
@@ -25,7 +28,8 @@ void initWifi() {
   }
   Serial.print("\nWiFi connected. IP address: ");
   Serial.println(WiFi.localIP());
-  configTime(1 * 3600, 0, "pool.ntp.org");      // get UTC time via NTP
+  // configTime(1 * 3600, 0, "pool.ntp.org");      // get UTC time via NTP
+  timeSync("UTC1", "pool.ntp.org", "time.nis.gov");
   while (!time(nullptr)) {
     delay(1000);
     Serial.println("Waiting for time synchronization...");
@@ -54,11 +58,54 @@ void bot_setup() {
   bot.setMyCommands(commands);
 }
 
-void sendSensorData(String payload) {
+void sendSensorData(
+  int sensor1value,
+  int sensor2value,
+  int sensor3value,
+  int sensor4value,
+  int sensor5value
+) {
   // Send sensor data to server by WiFi
   if (WiFi.status() != WL_CONNECTED) {
       reconnectWifi();
   }
+
+  // Create a JSON array to hold the sensors data
+  StaticJsonDocument<500> doc;
+  JsonArray sensors = doc.to<JsonArray>();
+
+  // Create the first sensor object
+  JsonObject sensor1 = sensors.createNestedObject();
+  sensor1["sensor"] = "sensor_1";
+  JsonObject data1 = sensor1.createNestedObject("data");
+  data1["moisture"] = sensor1value;
+
+  // Create the second sensor object
+  JsonObject sensor2 = sensors.createNestedObject();
+  sensor2["sensor"] = "sensor_2";
+  JsonObject data2 = sensor2.createNestedObject("data");
+  data2["moisture"] = sensor2value;
+
+  // Create the third sensor object
+  JsonObject sensor3 = sensors.createNestedObject();
+  sensor3["sensor"] = "sensor_3";
+  JsonObject data3 = sensor3.createNestedObject("data");
+  data3["moisture"] = sensor3value;
+
+  // Create the fourth sensor object
+  JsonObject sensor4 = sensors.createNestedObject();
+  sensor4["sensor"] = "sensor_4";
+  JsonObject data4 = sensor4.createNestedObject("data");
+  data4["moisture"] = sensor4value;
+
+  // Create the fifth sensor object
+  JsonObject sensor5 = sensors.createNestedObject();
+  sensor5["sensor"] = "sensor_5";
+  JsonObject data5 = sensor5.createNestedObject("data");
+  data5["moisture"] = sensor5value;
+
+  String payload;
+  serializeJson(doc, payload);
 
   // Init HTTP client
   WiFiClient wifiClient;
@@ -92,4 +139,25 @@ void sendBotMessage(
     String(sensorValue3) + ", " +
     String(sensorValue4) + ", " +
     String(sensorValue5), "Markdown");
+}
+
+void sendSensorDataToInfluxDB(const std::vector<int>& sensorValues) {
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnectWifi();
+  }
+
+  for (int i = 0; i < sensorValues.size(); ++i) {
+    InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+    Point sensor("sensors_data");
+    sensor.addTag("device", "ESP32");
+    sensor.addTag("sensor", "sensor_" + String(i + 1));
+    sensor.addField("moisture", sensorValues[i]);
+    // sensor.setTime(influxdb::InfluxDBUtils::getEpochTime());
+    if (!client.writePoint(sensor)) {
+      Serial.print("InfluxDB sensor_");
+      Serial.print(i + 1);
+      Serial.print(" write failed: ");
+      Serial.println(client.getLastErrorMessage());
+    }
+  }
 }
